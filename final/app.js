44e0c9258a0afa9a3e8ed4b71d9c970f2994ead1
Chris <54852438+CD00RT@users.noreply.github.com>
2020-09-16 17:53:33 +0200

    Update app.js

--- final/app.js
+++ final/app.js
@@ -109,7 +109,29 @@ function init() {
 
 
 
+/*
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    if(gamePlaying) {
+        // 1. Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
 
+        //2. Display the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+
+        //3. Update the round score IF the rolled number was NOT a 1
+        if (dice !== 1) {
+            //Add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            //Next player
+            nextPlayer();
+        }
+    }    
+});
 
 
 /*
